fddf6364c2aa9a611638491a2c11660fa90d3710
Ribal Aladeeb <aladeeb.r@hotmail.com>
2020-11-05 22:30:09 -0500

    Add a func to generate cost=1 type moves
    Co-authored-by: mohanadarafe <marafe@live.com>

--- board.py
+++ board.py
@@ -1,9 +1,11 @@
+from moves import Moves
 import numpy as np
+from typing import *
 
 
 class Board:
 
-    def __init__(self, shape=(2, 4), puzzle: np.array = None):
+    def __init__(self, puzzle: np.array = None):
         self.score = 0
         self.puzzle = puzzle
 
@@ -11,6 +13,80 @@ class Board:
         x = self.puzzle
         rows = x.shape[0]
         cols = x.shape[1]
-        goal_state = np.concatenate((np.arange(1,rows*cols),np.array([0])))
+        goal_state = np.concatenate((np.arange(1, rows*cols), np.array([0])))
 
         return np.array_equal(x.flatten(), goal_state) or np.array_equal(x.T.flatten(), goal_state)
+
+    def is_corner(self) -> bool:
+        position = np.where(self.puzzle == 0)
+        rows = position[0][0]
+        cols = position[1][0]
+
+        #puzzle = np.copy(self.puzzle)
+        #corners = puzzle[::puzzle.shape[0]-1, ::puzzle.shape[1]-1]
+        #corner_x, corner_y = np.where(corners == 0)[0][0]
+
+        self.is_corner.top_left = rows == 0 and cols == 0
+        self.is_corner.top_right = rows == 0 and cols == self.puzzle.shape[1] - 1
+        self.is_corner.bottom_right = rows == self.puzzle.shape[0] - 1 and cols == self.puzzle.shape[1] - 1
+        self.is_corner.bottom_left = rows == self.puzzle.shape[0] - 1 and cols == 0
+
+        return self.is_corner.top_left or self.is_corner.top_right or self.is_corner.bottom_left or self.is_corner.bottom_right
+
+    def _generate_regular_moves(self) -> List[dict]:
+        '''Make sure that this move is legal before performing it'''
+
+        possible_moves = ['up', 'down', 'left', 'right']
+        regular_moves = []
+
+        for move in possible_moves:
+            try:
+                rowof0 = np.where(self.puzzle == 0)[0][0]
+                colof0 = np.where(self.puzzle == 0)[1][0]
+                new_puzzle = np.copy(self.puzzle)
+                start = (rowof0, colof0)
+
+                if move == 'up':
+                    end = (rowof0-1, colof0)
+
+                elif move == 'down':
+                    end = (rowof0 + 1, colof0)
+
+                elif move == 'left':
+                    end = (rowof0, colof0-1)
+
+                else:  # move == 'right'
+                    end = (rowof0, colof0+1)
+
+                if end[0] < 0 or end[1] < 0:  # an index must be negative and we dont want that
+                    continue
+
+                new_puzzle[start], new_puzzle[end] = new_puzzle[end], new_puzzle[start]
+                regular_moves.append({'start': start, 'end': end, 'puzzle': new_puzzle, 'simple_cost': 1})
+
+            except IndexError:
+                continue
+
+        return regular_moves
+
+    def wrap_around(self, direction: str):
+        pass
+
+
+    def generate_wrapping_moves(self):
+        if not self.is_corner():
+            return None
+
+        rows = self.puzzle.shape[0]
+        cols = self.puzzle.shape[1]
+
+        can_be_performed = {
+            'left': self.is_corner.top_left or self.is_corner.bottom_left,
+            'right': self.is_corner.top_right or self.is_corner.bottom_right,
+            'up': (self.is_corner.top_left or self.is_corner.top_right) and 2 < rows,
+            'down': (self.is_corner.bottom_left or self.is_corner.bottom_right) and 2 < rows,
+        }
+
+        for move in can_be_performed:
+            if can_be_performed[move]:
+                self.wrap_around(move)
--- tests/test_board.py
+++ tests/test_board.py
@@ -17,3 +17,33 @@ def test_is_goal_state():
 
     assert actual_goal_1.is_goal_state()
     assert actual_goal_2.is_goal_state()
+
+
+def test_generate_regular_moves():
+    b = Board(puzzle=np.arange(8).reshape(2, 4))
+    print(f'initial puzzle\n{b.puzzle}')
+    print()
+
+    down_puzzle = np.copy(b.puzzle)
+    down_puzzle[(0, 0)], down_puzzle[(1, 0)] = down_puzzle[(1, 0)], down_puzzle[(0, 0)]
+    print(f'puzzle after moving tile down\n{down_puzzle}')
+    print()
+
+    right_puzzle = np.copy(b.puzzle)
+    right_puzzle[(0, 0)], right_puzzle[(0, 1)] = right_puzzle[(0, 1)], right_puzzle[(0, 0)]
+    print(f'puzzle after moving tile right\n{right_puzzle}')
+
+    expected_result = [
+        {"start": (0, 0), "end": (1, 0), "puzzle": down_puzzle, "simple_cost": 1},
+        {"start": (0, 0), "end": (0, 1), "puzzle": right_puzzle, "simple_cost": 1}
+    ]
+
+    actual_result = b._generate_regular_moves()
+
+    index = 0
+    for results in actual_result:
+        print(results)
+        print("\n")
+        print(expected_result[index])
+        assert (results == expected_result[index]).all()
+        index += 1
